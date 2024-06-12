110afe60bd838de309da8f31242afcf9fb4a948c
Ange_Tech(Angel_Molina) <166965796+AngelTech90@users.noreply.github.com>
2024-06-11 19:33:42 -0600

    New notes un php basic features

---
+++ notes.php
@@ -0,0 +1,7 @@
+//*We have like commands in our php terminal commands like php help, that show use diffent utilities for work with the language */
+
+<?php
+
+    echo "Hello world";//* Our first hello world in php (echo let us write anything in our work environment).
+
+?>
